9120816c6189b6f859fb7caaf392c24a55a0de9d
lriccardo <32816743+lriccardo@users.noreply.github.com>
2017-12-11 16:35:18 +0100

    update

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
- var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    setWrapperSize: true,
+ var mySwiper = new Swiper('.swiper-container', {
+     // Optional parameters
+     direction: 'horizontal',
+     setWrapperSize: true,
      loop: false,
      preloadImages: true,
-     
- effect: 'fade',
+
+     effect: 'fade',
      crossFade: true,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-        dynamicBullets: true,
-        
-    },
+     // If we need pagination
+     pagination: {
+         el: '.swiper-pagination',
+         dynamicBullets: true,
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+     },
 
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
+     // Navigation arrows
+     navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+     },
+
+     // And if we need scrollbar
+     scrollbar: {
+         el: '.swiper-scrollbar',
+     },
+ })
